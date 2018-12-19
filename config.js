aca810b04359e8a695326a4b7e3e483f5cf68de9
Gerome Heath <gerome.heath@gmail.com>
2018-12-19 10:23:24 +0000

    Added frontend folder to guthub

---
+++ config.js
@@ -0,0 +1,2 @@
+//export const Config = {apiUrl: 'http://localhost:8080'}
+export const Config = {apiUrl: 'http://www.geromeheath.co.uk'}
---
+++ pages/post-index.js
@@ -0,0 +1,41 @@
+
+import React, { Component } from 'react'
+import Layout from '../components/Layout.js';
+import PostIndex from '../components/PostIndex'
+import fetch from "isomorphic-unfetch";
+import PageWrapper from "../components/PageWrapper.js";
+import { Config } from '../config';
+
+class PostIndexPage extends Component {
+  //getInitialProps Next JS
+  static async getInitialProps() {
+    const postsRes = await fetch(`${Config.apiUrl}/wp-json/wp/v2/posts`);
+    const posts = await postsRes.json();
+    return {
+      posts
+    };
+  }
+
+  render() {
+    const { posts } = this.props;
+    return (
+      <Layout
+      index={false}
+      {...this.props}
+      >
+
+        <h1>Post Index</h1>
+        <PostIndex limit={20}/>
+        {/* <ul>
+        {posts.map(post => (
+          <li>
+            {post.title.rendered}
+          </li>
+        ))}
+        </ul> */}
+      </Layout>
+    )
+  }
+}
+
+export default PageWrapper(PostIndexPage);
